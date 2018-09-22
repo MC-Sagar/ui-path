2c3bd2be33bd8b6b5ee9fde836deb1a2318f5880
Kartik <kartik.koolks@gmail.com>
2018-09-23 05:02:35 +0530

    changed code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const restService = express();
 var querystring = require("querystring");
 const Http = require("http");
+
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -42,19 +43,32 @@ const postUrl='https://platform.uipath.com/api/account/authenticate';
 // });
 
 //add querystring.stringify({})
-var post_data = {
+var post_data = querystring.stringify({
   "tenancyName" : "KartikKulks",
   "usernameOrEmailAddress" : "kartik.koolks@gmail.com",
   "password" : "Passw0rd"
-};
-var post_options = {
-  uri: postUrl,
-  method: 'POST',
-  json:post_data,
-};
-request(post_options,function(error,presp, pbody){
-  res.status(200).send(pbody);
 });
+var post_options = {
+  host: postUrl,
+  method: 'POST',
+  headers:{'Content-Type':'application/x-www-form-urlencoded',
+  'Content-Length': Buffer.byteLength(post_data)}
+};
+
+var post_req = http.request(post_options,function(res){
+  res.setEncoding('utf8');
+  res.on('data',function(chunk){
+    console.log('Response' + chunk);
+  });
+});
+
+post_req.write(post_data);
+console.log(post_data);
+
+//
+// request(post_options,function(error,presp, pbody){
+//   res.status(200).send(pbody);
+// });
 //
 //
 // Http.onreadystatechange = function() {
@@ -71,14 +85,17 @@ request(post_options,function(error,presp, pbody){
 // });
 
 //Bearer token which gets expired after 30 mins
-Http.setRequestHeader('Authorization','Bearer ' + token);
-Http.open("POST",post_jobs,true);
-Http.setRequestHeader('Content-Type', 'application/json');
-Http.send(JSON.stringify({
- "startInfo": {
-   "ReleaseKey": "dd0518b0-135b-46a3-8787-c796431f9b8e"
- }
-}));
+
+
+//
+// Http.setRequestHeader('Authorization','Bearer ' + token);
+// Http.open("POST",post_jobs,true);
+// Http.setRequestHeader('Content-Type', 'application/json');
+// Http.send(JSON.stringify({
+//  "startInfo": {
+//    "ReleaseKey": "dd0518b0-135b-46a3-8787-c796431f9b8e"
+//  }
+// }));
 
 
   return res.json({
@@ -86,6 +103,8 @@ Http.send(JSON.stringify({
     displayText: "test for fallback error done",
     source: "webhook-echo-sample"
   });
+
+  //till here
 });
 
 restService.listen(process.env.PORT || 8000, function() {
