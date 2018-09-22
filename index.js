7580d9f3514c8161871f372150ea08908741c901
Kartik <kartik.koolks@gmail.com>
2018-09-22 23:54:27 +0530

    lol

--- index.js
+++ index.js
@@ -14,37 +14,41 @@ server.use(bodyParser.json());
 
 
 
- const Http = new XMLHttpRequest();
- const postUrl='https://platform.uipath.com/api/account/authenticate';
- Http.open("POST",postUrl,true);
- Http.setRequestHeader('Content-Type', 'application/json');
- Http.send(JSON.stringify({
-    "tenancyName" : "KartikKulks",
-    "usernameOrEmailAddress" : "kartik.koolks@gmail.com",
-    "password" : "Passw0rd"
- }));
- resp_bearer = Http.responseText.result
-
-
-
- post_jobs='https://platform.uipath.com/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs'
-
- Http.setRequestHeader('Authorization','Bearer ' + resp_bearer)
- Http.open("POST",post_jobs,true);
- Http.setRequestHeader('Content-Type', 'application/json');
-
-
- Http.setRequestHeader('Authorization', 'Bearer ' + resp_bearer);
- Http.setRequestHeader('Content-Type','application/json');
- Http.send(JSON.stringify({
-  "startInfo": {
-    "ReleaseKey": "dd0518b0-135b-46a3-8787-c796431f9b8e"
-  }
-}))
-
-
-
 server.post('/getError',function (request,response)  {
+
+
+
+   //const Http = new XMLHttpRequest();
+   const Http = require("xmlhttprequest").XMLHttpRequest;
+   //const Http = new XMLHttpRequest();
+   const postUrl='https://platform.uipath.com/api/account/authenticate';
+   Http.open("POST",postUrl,true);
+   Http.setRequestHeader('Content-Type', 'application/json');
+   Http.send(JSON.stringify({
+      "tenancyName" : "KartikKulks",
+      "usernameOrEmailAddress" : "kartik.koolks@gmail.com",
+      "password" : "Passw0rd"
+   }));
+   resp_bearer = Http.responseText.result
+
+
+
+   post_jobs='https://platform.uipath.com/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs'
+
+   Http.setRequestHeader('Authorization','Bearer ' + resp_bearer)
+   Http.open("POST",post_jobs,true);
+   Http.setRequestHeader('Content-Type', 'application/json');
+
+
+   Http.setRequestHeader('Authorization', 'Bearer ' + resp_bearer);
+   Http.setRequestHeader('Content-Type','application/json');
+   Http.send(JSON.stringify({
+    "startInfo": {
+      "ReleaseKey": "dd0518b0-135b-46a3-8787-c796431f9b8e"
+    }
+  }))
+
+
 
 
   response.setHeader('Content-Type','application/json');
